726bd42e17036619e789374d944ce762fdaadd17
Rida Abdallah <euro_rabdallah@aaib.com>
2023-01-27 11:40:25 +0200

    Updates

---
+++ precache-manifest.4da9fdc9bc4ec1ea7a7d94b1e26b06f8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36807a3d37c96edc54303ac6825bf3a9",
+    "url": "/nutritionWebSite/index.html"
+  },
+  {
+    "revision": "0c5d6ed4cb9b06f1eb0b",
+    "url": "/nutritionWebSite/static/css/main.b5a79f6f.chunk.css"
+  },
+  {
+    "revision": "b4ed4676e3ea1886247d",
+    "url": "/nutritionWebSite/static/js/2.0fe70d91.chunk.js"
+  },
+  {
+    "revision": "418b1618ef813639db7916e4a68a6a2a",
+    "url": "/nutritionWebSite/static/js/2.0fe70d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5d6ed4cb9b06f1eb0b",
+    "url": "/nutritionWebSite/static/js/main.25773035.chunk.js"
+  },
+  {
+    "revision": "aa44f01faba6a8d6cd48",
+    "url": "/nutritionWebSite/static/js/runtime-main.e463e8c0.js"
+  },
+  {
+    "revision": "cdb3f3644e5fb606c56b7816efa47793",
+    "url": "/nutritionWebSite/static/media/1.cdb3f364.jpg"
+  },
+  {
+    "revision": "6fa164d133f0ce4bc722aadd54352642",
+    "url": "/nutritionWebSite/static/media/2.6fa164d1.jpg"
+  },
+  {
+    "revision": "5e1390c2ee0f6d8a7b66d1a36a71f205",
+    "url": "/nutritionWebSite/static/media/3.5e1390c2.jpg"
+  },
+  {
+    "revision": "9bf1baa9ba7290b4ea49c21d6d0df9e8",
+    "url": "/nutritionWebSite/static/media/4.9bf1baa9.jpg"
+  },
+  {
+    "revision": "44c1ae72a84350ca6997560601185451",
+    "url": "/nutritionWebSite/static/media/5.44c1ae72.jpg"
+  },
+  {
+    "revision": "cf7f2b0640dfe908b8cde51c72a65cf2",
+    "url": "/nutritionWebSite/static/media/8.cf7f2b06.jpg"
+  },
+  {
+    "revision": "a8de8ffe262db7e277deb8f1c439c518",
+    "url": "/nutritionWebSite/static/media/Montserrat-Bold.a8de8ffe.ttf"
+  },
+  {
+    "revision": "8777b44e5f19cd64f83171126db732f0",
+    "url": "/nutritionWebSite/static/media/Montserrat-Italic.8777b44e.ttf"
+  },
+  {
+    "revision": "f7213526ec9296ff43426bfe3eae8926",
+    "url": "/nutritionWebSite/static/media/Montserrat-Regular.f7213526.ttf"
+  },
+  {
+    "revision": "ca304f5348de07cd6c45f6bd5018f9cb",
+    "url": "/nutritionWebSite/static/media/appLogo.ca304f53.svg"
+  },
+  {
+    "revision": "999b158f9c1a4f2e268734fbf2943519",
+    "url": "/nutritionWebSite/static/media/hananPhoto.999b158f.jpg"
+  },
+  {
+    "revision": "c7e20cbe3187d2f8f02995e006f65d34",
+    "url": "/nutritionWebSite/static/media/home.c7e20cbe.jpg"
+  },
+  {
+    "revision": "cc3ac5f8f5deccd2cedc87182220947e",
+    "url": "/nutritionWebSite/static/media/night.cc3ac5f8.jpg"
+  }
+]);
