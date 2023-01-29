825820df6a3827b3a24f03a8022a40e02d823ce3
Rida Abdallah <euro_rabdallah@aaib.com>
2023-01-29 10:31:01 +0200

    feat: website

--- src/styled/navBar/navBar.component.tsx
+++ src/styled/navBar/navBar.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./navBar.module.css";
 
 import { ReactComponent as AppLogo } from "&assets/images/appLogo.svg";
@@ -7,12 +7,33 @@ import { Link } from "react-router-dom";
 
 interface Props {}
 
-const NavBar = (props: Props) => {
+const NavBar = () => {
   const [clicked, setClicked] = useState(false);
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const controlNavbar = () => {
+      if (typeof window !== "undefined") {
+        if (window.scrollY > lastScrollY) {
+          setShow(false);
+        } else {
+          setShow(true);
+        }
+
+        setLastScrollY(window.scrollY);
+      }
+    };
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
 
   return (
-    <nav className={styles.navBarItems}>
-      {/* <h1 className={styles.navbarLogo}>Hanan Hijazi</h1> */}
+    <nav className={show ? styles.navBarItems : styles.navBarItemsHide}>
       <div className={styles.logoContainer}>
         <AppLogo className={styles.logo} />
       </div>
